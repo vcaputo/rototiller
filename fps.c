e3d3c06aa5b47648c0ed145279d7950df42ad80d
Vito Caputo <vcaputo@gnugeneration.com>
2016-11-23 19:17:46 -0800

    fps: add rudimentary FPS counter
    This is just the implementation, nothing leveraged yet.

---
+++ fps.c
@@ -0,0 +1,46 @@
+#include <signal.h>
+#include <stdio.h>
+#include <sys/time.h>
+
+#include "fb.h"
+#include "util.h"
+
+
+static int	print_fps;
+
+
+static void sigalrm_handler(int signum)
+{
+	print_fps = 1;
+}
+
+
+int fps_setup(void)
+{
+	struct itimerval	interval = { 	
+					.it_interval = { .tv_sec = 1, .tv_usec = 0 },
+					.it_value = { .tv_sec = 1, .tv_usec = 0 },
+				};
+
+	if (signal(SIGALRM, sigalrm_handler) == SIG_ERR)
+		return 0;
+		
+	if (setitimer(ITIMER_REAL, &interval, NULL) < 0)
+		return 0;
+
+	return 1;
+}
+
+
+void fps_print(fb_t *fb)
+{
+	unsigned	n;
+
+	if (!print_fps)
+		return;
+
+	fb_get_put_pages_count(fb, &n);
+	printf("FPS: %u\n", n);
+
+	print_fps = 0;
+}
---
+++ fps.h
@@ -0,0 +1,9 @@
+#ifndef _FPS_H
+#define _FPS_H
+
+#include "fb.h"
+
+int fps_setup(void);
+void fps_print(fb_t *fb);
+
+#endif
