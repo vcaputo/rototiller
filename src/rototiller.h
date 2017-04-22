5ba408fd9ce88a3635f745930cf09cb47a1400a2
Vito Caputo <vcaputo@pengaru.com>
2017-04-22 08:00:22 -0700

    rototiller: add prepare_frame & rototiller_frame_t
    Undecided on wether rototiller_frame_t should be fb_frame_t or not,
    may change later.

--- src/rototiller.h
+++ src/rototiller.h
@@ -1,8 +1,19 @@
 #ifndef _ROTOTILLER_H
 #define _ROTOTILLER_H
 
+#include "fb.h"
+
+/* Intentionally setting this larger than any anticipated number of CPUs */
+#define ROTOTILLER_FRAME_MAX_FRAGMENTS	1024
+
+typedef struct rototiller_frame_t {
+	unsigned	n_fragments;
+	fb_fragment_t	fragments[ROTOTILLER_FRAME_MAX_FRAGMENTS];
+} rototiller_frame_t;
+
 typedef struct rototiller_module_t {
-	void	(*render_fragment)(fb_fragment_t *);
+	void	(*prepare_frame)(unsigned n_cpus, fb_fragment_t *fragment, rototiller_frame_t *res_frame);
+	void	(*render_fragment)(fb_fragment_t *fragment);
 	char	*name;
 	char	*description;
 	char	*author;
