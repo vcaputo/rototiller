e1276c6e2cdf32772917f903467beafb692bb6ae
Vito Caputo <vcaputo@pengaru.com>
2017-05-27 14:47:58 -0700

    ray: inline ray_object_* switch functions

--- src/modules/ray/ray_object.h
+++ src/modules/ray/ray_object.h
@@ -1,6 +1,8 @@
 #ifndef _RAY_OBJECT_H
 #define _RAY_OBJECT_H
 
+#include <assert.h>
+
 #include "ray_object_light.h"
 #include "ray_object_plane.h"
 #include "ray_object_point.h"
@@ -17,9 +19,94 @@ typedef union ray_object_t {
 	ray_object_light_t	light;
 } ray_object_t;
 
-void ray_object_prepare(ray_object_t *object);
-int ray_object_intersects_ray(ray_object_t *object, ray_ray_t *ray, float *res_distance);
-ray_3f_t ray_object_normal(ray_object_t *object, ray_3f_t *point);
-ray_surface_t ray_object_surface(ray_object_t *object, ray_3f_t *point);
+
+
+/* Prepare an object for rendering.
+ * If the object has any pre-calculating to do, this is where it happens.
+ * The pre-calculated stuff is object-resident under a _prepared struct member.
+ */
+static inline void ray_object_prepare(ray_object_t *object)
+{
+	switch (object->type) {
+	case RAY_OBJECT_TYPE_SPHERE:
+		return ray_object_sphere_prepare(&object->sphere);
+
+	case RAY_OBJECT_TYPE_POINT:
+		return ray_object_point_prepare(&object->point);
+
+	case RAY_OBJECT_TYPE_PLANE:
+		return ray_object_plane_prepare(&object->plane);
+
+	case RAY_OBJECT_TYPE_LIGHT:
+		return ray_object_light_prepare(&object->light);
+	default:
+		assert(0);
+	}
+}
+
+
+/* Determine if a ray intersects object.
+ * If the object is intersected, store where along the ray the intersection occurs in res_distance.
+ */
+static inline int ray_object_intersects_ray(ray_object_t *object, ray_ray_t *ray, float *res_distance)
+{
+	switch (object->type) {
+	case RAY_OBJECT_TYPE_SPHERE:
+		return ray_object_sphere_intersects_ray(&object->sphere, ray, res_distance);
+
+	case RAY_OBJECT_TYPE_POINT:
+		return ray_object_point_intersects_ray(&object->point, ray, res_distance);
+
+	case RAY_OBJECT_TYPE_PLANE:
+		return ray_object_plane_intersects_ray(&object->plane, ray, res_distance);
+
+	case RAY_OBJECT_TYPE_LIGHT:
+		return ray_object_light_intersects_ray(&object->light, ray, res_distance);
+	default:
+		assert(0);
+	}
+}
+
+
+/* Return the surface normal of object @ point */
+static inline ray_3f_t ray_object_normal(ray_object_t *object, ray_3f_t *point)
+{
+	switch (object->type) {
+	case RAY_OBJECT_TYPE_SPHERE:
+		return ray_object_sphere_normal(&object->sphere, point);
+
+	case RAY_OBJECT_TYPE_POINT:
+		return ray_object_point_normal(&object->point, point);
+
+	case RAY_OBJECT_TYPE_PLANE:
+		return ray_object_plane_normal(&object->plane, point);
+
+	case RAY_OBJECT_TYPE_LIGHT:
+		return ray_object_light_normal(&object->light, point);
+	default:
+		assert(0);
+	}
+}
+
+
+/* Return the surface of object @ point */
+static inline ray_surface_t ray_object_surface(ray_object_t *object, ray_3f_t *point)
+{
+	switch (object->type) {
+	case RAY_OBJECT_TYPE_SPHERE:
+		return ray_object_sphere_surface(&object->sphere, point);
+
+	case RAY_OBJECT_TYPE_POINT:
+		return ray_object_point_surface(&object->point, point);
+
+	case RAY_OBJECT_TYPE_PLANE:
+		return ray_object_plane_surface(&object->plane, point);
+
+	case RAY_OBJECT_TYPE_LIGHT:
+		return ray_object_light_surface(&object->light, point);
+	default:
+		assert(0);
+	}
+}
 
 #endif
