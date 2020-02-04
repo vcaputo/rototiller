6cff7d0993de7d059c52ce737dca398d380dec88
Vito Caputo <vcaputo@pengaru.com>
2020-02-03 16:48:06 -0800

    libs/sig: s/sig_ops_inv/sig_ops_neg/g
    Rename inv->neg, preparation for a new sig_ops_inv for inverting
    0..1 to 1..0

---
+++ src/libs/sig/ops_neg.c
@@ -0,0 +1,52 @@
+#include <assert.h>
+
+#include "sig.h"
+
+
+typedef struct ops_neg_ctxt_t {
+	sig_t	*x;
+} ops_neg_ctxt_t;
+
+
+static size_t ops_neg_size(va_list ap)
+{
+	return sizeof(ops_neg_ctxt_t);
+}
+
+
+static void ops_neg_init(void *context, va_list ap)
+{
+	ops_neg_ctxt_t	*ctxt = context;
+
+	assert(ctxt);
+
+	ctxt->x = va_arg(ap, sig_t *);
+}
+
+
+static void ops_neg_destroy(void *context)
+{
+	ops_neg_ctxt_t	*ctxt = context;
+
+	assert(ctxt);
+
+	sig_free(ctxt->x);
+}
+
+
+static float ops_neg_output(void *context, unsigned ticks_ms)
+{
+	ops_neg_ctxt_t	*ctxt = context;
+
+	assert(ctxt);
+
+	return -sig_output(ctxt->x, ticks_ms);
+}
+
+
+sig_ops_t	sig_ops_neg = {
+	.size = ops_neg_size,
+	.init = ops_neg_init,
+	.destroy = ops_neg_destroy,
+	.output = ops_neg_output,
+};
