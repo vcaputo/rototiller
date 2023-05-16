vcaputo/rototiller

commit d72e924a845a2412d2106dc3fe0b9d59fb7faf59

til: omit experimental modules from til_module_setup()
Don't make experimental modules available to the
regular/potentially-interactive setup routine.

There should be a flag like --experimental to generally enable
these.

src/til.c
@@ -281,6 +281,9 @@ int til_module_setup(til_settings_t *settings, til_setting_t **res_setting, cons
 		int			r;
 
 		for (unsigned i = 0; i < nelems(modules); i++) {
+			if ((modules[i]->flags & TIL_MODULE_EXPERIMENTAL))
+				continue;
+
 			values[i] = modules[i]->name;
 			annotations[i] = modules[i]->description;
 		}

commit b07dcecc6ae6abdc9ff7fab0179475903740ce43

til: fix til_setup_module() skip-experimental bug
Commit d72e924a introduced skipping experimental modules for the
values, but it didn't bring in a new index for storing the values
so the offset w/skips was being used to store the values too.

This created a sparse set of values for the setup, manifesting as
a wonky partial interactive setup for the module that didn't even
include the default 0 (rtv) value in the list.

Simple fix.

src/til.c
@@ -278,12 +278,13 @@ int til_module_setup(const til_settings_t *settings, til_setting_t **res_setting
 		til_setting_desc_t	*desc;
 		int			r;
 
-		for (unsigned i = 0; i < nelems(modules); i++) {
+		for (unsigned i = 0, j = 0; i < nelems(modules); i++) {
 			if ((modules[i]->flags & TIL_MODULE_EXPERIMENTAL))
 				continue;
 
-			values[i] = modules[i]->name;
-			annotations[i] = modules[i]->description;
+			values[j] = modules[i]->name;
+			annotations[j] = modules[i]->description;
+			j++;
 		}
 
 		r = til_setting_desc_new(	settings,